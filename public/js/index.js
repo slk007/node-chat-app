da7d44fd5da27b720b8dcf9a63175d630216a83c
slk007 <shubham.soni.lko@gmail.com>
2018-03-30 22:12:49 +0530

    emit newMessage in createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'soni',
-		text: 'A message was just created'
-	});
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected');
 
-	socket.emit('newMessage', {
-		from: 'shubham',
-		text:'Hello how are you',
-		createdAt: 1234
-	});
-
-	socket.on('createMessage', (newMessage) => {
-		console.log('createMessage', newMessage);
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
 	});
 
 	socket.on('disconnect', () => {
