7a0b76a9cdde20ae33a819e47d034431256591b7
slk007 <shubham.soni.lko@gmail.com>
2018-03-30 19:41:22 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express');
-let app = express();
+const socketIO = require('socket.io');
+
 
 const port = process.env.PORT || 3000;
 const  publicPath = path.join(__dirname, '../public');
+let app = express();
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-	res.render('index.html');
-});
+// app.get('/', (req, res) => {
+// 	res.render('index.html');
+// });
 
 
 app.listen(port, () => {
