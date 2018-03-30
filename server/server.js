c39935c67d7d217fb5d72eaeb8e767f779eb6906
slk007 <shubham.soni.lko@gmail.com>
2018-03-30 22:52:23 +0530

    broadcasting

--- server/server.js
+++ server/server.js
@@ -15,13 +15,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat room',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New User joined',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
-		})
+		});
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
