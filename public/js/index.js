eae76e374fcea782a536cdfd8c3e56d72cb6d52b
slk007 <shubham.soni.lko@gmail.com>
2018-03-31 23:15:30 +0530

    adding templating for messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,33 +10,38 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  li.text(`${message.from}: ${formattedTime}: ${message.text}`);
+  let template = jQuery('#message-template').html();
+  let html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
-socket.on('newLocationMessage', function (message){
+socket.on('newLocationMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-	let li  =jQuery('<li></li>');
-	let a = jQuery('<a target="_blank">My current location</a>');
+  let template = jQuery('#location-message-template').html();
+  let html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-	li.text(`${message.from}: ${formattedTime}: `);
-	a.attr('href', message.url);
-	li.append(a);
-  	jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  let messageTextBox = jQuery('[name=message]');
+  let messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: messageTextBox.val()
+    text: messageTextbox.val()
   }, function () {
-  	messageTextBox.val('')
+    messageTextbox.val('')
   });
 });
 
@@ -46,16 +51,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr('disabled','disabled').text('Sending location...');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
-  	locationButton.removeAttr('disabled').text('Sending location');
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
-  	locationButton.removeAttr('disabled').text('Sending location');
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
