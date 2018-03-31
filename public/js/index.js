138c25378b6b010d72ecb7aff4478dea04016302
slk007 <shubham.soni.lko@gmail.com>
2018-03-31 23:35:12 +0530

    auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom(){
+
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = messages.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
