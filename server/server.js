60d99f5a2a0998049d9ee0c133da8e6bad6c58ef
slk007 <shubham.soni.lko@gmail.com>
2018-03-31 14:37:48 +0530

    adding form for messages

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ io.on('connection', (socket) => {
 
 	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', generateMessage(message.from, message.text));
-
+		callback('This is from the server.');
 		// socket.broadcast.emit('newMessage', {
 		// 	from: message.from,
 		// 	text: message.text,
