06a58ab0dad5336e7007e09459bbc94075ba5d83
slk007 <shubham.soni.lko@gmail.com>
2018-04-01 15:20:43 +0530

    add private messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   let messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
